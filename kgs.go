6bfccc9b4c8b6a7f1173e18948a6548944c39c08
CoilDomain <xorphenochx@gmail.com>
2020-10-23 20:16:29 +0000

    KGS basic function names

--- kgs.go
+++ kgs.go
@@ -1,1 +1,25 @@
 package gGoban
+
+func kgsLogin() {
+
+}
+
+func kgsPoll() {
+
+}
+
+func kgsSendMesg() {
+
+}
+
+func kgsRecMesg() {
+
+}
+
+func kgsNewGameReq() {
+
+}
+
+func kgsListGames() {
+
+}
